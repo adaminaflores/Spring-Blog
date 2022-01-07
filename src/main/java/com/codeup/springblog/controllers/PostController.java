3d7cf0134c56492ec0121318ba1a45aa870dcd2a
Adamina Flores <adaminaflores19@gmail.com>
2022-01-07 12:21:59 -0600

    finished the views exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,23 +1,35 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String indexPage(){
-        return "This is the index page!";
+    public String indexPage(Model model){
+        Post newPost1 = new Post("Post 1", "this is post 1");
+        Post newPost2 = new Post("Post 2", "this is post 2");
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        posts.add(newPost1);
+        posts.add(newPost2);
+
+        model.addAttribute("allPosts", posts);
+
+        return "posts/index";
     }
 
-    @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String singlePost(@PathVariable String id){
-        return "view an individual post" + id;
+    @GetMapping("/posts/show")
+    public String singlePost(Model viewModel){
+        Post newPost =  new Post("Hello World", "Welcome to Codeup!");
+        viewModel.addAttribute("post", newPost);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
